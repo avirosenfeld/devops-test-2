ff55e2df5c16f5a6c11e42f281aebc777e83f448
avirosenfeld <avi.rosenfeld@gmail.com>
2022-02-22 09:48:45 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,19 +11,46 @@ resource "aws_subnet" "subnet-a" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = var.subnet-cidr-a
   availability_zone = "${var.region}a"
+
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "subnet-b" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = var.subnet-cidr-b
   availability_zone = "${var.region}b"
+
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "subnet-c" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = var.subnet-cidr-c
   availability_zone = "${var.region}c"
+
+  map_public_ip_on_launch = true
 }
+
+
+resource "aws_subnet" "private-subnet-a" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.private-subnet-cidr-a
+  availability_zone = "${var.region}a"
+}
+
+resource "aws_subnet" "private-subnet-b" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.private-subnet-cidr-b
+  availability_zone = "${var.region}b"
+}
+
+resource "aws_subnet" "private-subnet-c" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.private-subnet-cidr-c
+  availability_zone = "${var.region}c"
+}
+
+
 
 resource "aws_route_table" "subnet-route-table" {
   vpc_id = aws_vpc.vpc.id
@@ -33,12 +60,15 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
 }
 
+
 resource "aws_route" "subnet-route" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
   route_table_id         = aws_route_table.subnet-route-table.id
 }
 
+
+/*
 resource "aws_route_table_association" "subnet-a-route-table-association" {
   subnet_id      = aws_subnet.subnet-a.id
   route_table_id = aws_route_table.subnet-route-table.id
@@ -53,16 +83,40 @@ resource "aws_route_table_association" "subnet-c-route-table-association" {
   subnet_id      = aws_subnet.subnet-c.id
   route_table_id = aws_route_table.subnet-route-table.id
 }
+*/
+
+data "aws_ami" "amazon-linux-2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
 
 resource "aws_instance" "instance" {
-  ami                         = "ami-085ed5922c6881dd6"
-  instance_type               = "t2.small"
-  vpc_security_group_ids      = [ aws_security_group.security-group.id ]
-  subnet_id                   = aws_subnet.subnet-a.id
+  ami                         = data.aws_ami.amazon-linux-2.id
+  instance_type               = "t2.micro"
+  vpc_security_group_ids      = [aws_security_group.security-group-private.id]
+  subnet_id                   = aws_subnet.private-subnet-a.id
   associate_public_ip_address = true
   user_data                   = <<EOF
 #!/bin/sh
-yum install -y nginx
+amazon-linux-extras install nginx1
+service nginx start
+EOF
+}
+
+resource "aws_instance" "instance2" {
+  ami                         = data.aws_ami.amazon-linux-2.id
+  instance_type               = "t2.micro"
+  vpc_security_group_ids      = [aws_security_group.security-group-private.id]
+  subnet_id                   = aws_subnet.private-subnet-b.id
+  associate_public_ip_address = true
+  user_data                   = <<EOF
+#!/bin/sh
+amazon-linux-extras install nginx1
 service nginx start
 EOF
 }
@@ -74,26 +128,56 @@ resource "aws_security_group" "security-group" {
     from_port   = "80"
     to_port     = "80"
     protocol    = "tcp"
-    cidr_blocks = [ "0.0.0.0/0" ]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port   = "443"
     to_port     = "443"
     protocol    = "tcp"
-    cidr_blocks = [ "0.0.0.0/0" ]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port   = "22"
     to_port     = "22"
     protocol    = "tcp"
-    cidr_blocks = [ "0.0.0.0/0" ]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = [ "0.0.0.0/0" ]
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "security-group-private" {
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = "80"
+    to_port     = "80"
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.vpc.id]
+  }
+  ingress {
+    from_port   = "443"
+    to_port     = "443"
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.vpc.id]
+  }
+  ingress {
+    from_port   = "22"
+    to_port     = "22"
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.vpc.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
